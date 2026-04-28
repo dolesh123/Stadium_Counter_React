d7ed60a764d5d4386d452985ff3a4d31bf33babc
Peddaboina Doleshwar <128211653+dolesh123@users.noreply.github.com>
2026-04-28 17:37:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🏟️ Stadium Counter</h1>
+      <h2>People Count: {count}</h2>
+
+      <button onClick={increase} style={{ margin: "10px" }}>
+        + Enter
+      </button>
+
+      <button onClick={decrease} style={{ margin: "10px" }}>
+        - Exit
+      </button>
     </div>
   );
 }
